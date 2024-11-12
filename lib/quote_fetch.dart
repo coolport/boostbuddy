719abb0a5b7f665981284ea3401c0ed074212689
coolport <aidanalcayde@gmail.com>
2024-11-12 15:55:41 +0800

    added: api fetching service

---
+++ lib/quote_fetch.dart
@@ -0,0 +1,37 @@
+// quote_service.dart
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class QuoteService {
+  static Future<Map<String, String>> fetchQuote() async {
+    final urlQuotes = Uri.parse('https://api.api-ninjas.com/v1/quotes?category=happiness');
+    var headers = {
+      'X-Api-Key': '22Sw/FqKs4++B+lC243/VQ==xiOpF8RKMO5PxxLI'
+    };
+
+    final urlQuotesResponse = await http.get(urlQuotes, headers: headers);
+
+    if (urlQuotesResponse.statusCode == 200) {
+      final json = jsonDecode(urlQuotesResponse.body);
+      final quoteData = json.isNotEmpty ? json[0] : null;
+
+      if (quoteData != null) {
+        return {
+          'quote': quoteData['quote'],
+          'author': quoteData['author'],
+        };
+      } else {
+        return {
+          'quote': 'No quotes found.',
+          'author': '',
+        };
+      }
+    } else {
+      return {
+        'quote': 'Failed to load quote.',
+        'author': '',
+      };
+    }
+  }
+}
